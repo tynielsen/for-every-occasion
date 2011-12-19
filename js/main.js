7dd3bd85790d8ea2777ea1b46b1a7f4482c85c3e
Tyler Nielsen <tylern@mediarain.com>
2011-12-18 22:52:14 -0700

    optimiazing css and js files, removing duplicate gallery photos

--- js/main.js
+++ js/main.js
@@ -1,24 +1,20 @@
 function slideSwitch() {
-	//Fade effect for the home page main image carousel
-    var $active = $('.image-carousel li.active');
+	// Fade effect for home page image carousel
+	var $active = $('.image-carousel li.active');
 
-    if ( $active.length == 0 ) $active = $('.image-carousel li:last');
+	if ($active.length == 0 ) $active = $('.image-carousel li:last');
 
-    var $next = $active.next().length ? $active.next()
-        : $('.image-carousel li:first');
+	var $next = $active.next().length ? $active.next() : $('.image-carousel li:first');
 
-    $active.addClass('last-active');
+	$active.addClass('last-active');
 
-    $next.css({opacity: 0.0})
-        .addClass('active')
-        .animate({opacity: 1.0}, 1000, function() {
-            $active.removeClass('active last-active');
-        });
+	$next.css({opacity: 0.0}).addClass('active').animate({opacity: 1.0}, 1000, function() {
+		$active.removeClass('active last-active');
+	});
 }
 $(function(){
 	//setting timer for homepage image carousel
-	//var seconds = 8 /*<--only change this number*/ * 1000;
-    setInterval( "slideSwitch()", 5000);
+	setInterval( "slideSwitch()", 5000);
 
 	//functionality for the Gallery Page gallery
 	//$('img.image1').data('ad-desc', 'Whoa! This description is set through elm.data("ad-desc") instead of using the longdesc attribute.<br>And it contains <strong>H</strong>ow <strong>T</strong>o <strong>M</strong>eet <strong>L</strong>adies... <em>What?</em> That aint what HTML stands for? Man...');
